f89d5f26c6231b1a1e4109476a7527c25740c48b
RShGeFu <RShGeFu@users.noreply.github.com>
2017-08-07 18:23:44 +0200

    Einrichten der ersten Panels des UI Test-Panels für einzelne Modultests

--- server.R
+++ server.R
@@ -7,4 +7,29 @@ library(shiny)
 
 shinyServer(function(input, output, session) {
   
+  # --- Bei Sessionende ... --------------------------------------------------------------------------------
+  session$onSessionEnded(function() {
+    # ggf. nochmaliges Speichern
+    print("(c) SchafkopfApp by G. Füchsl - Auf Wiedersehen, bis zum nächsten Mal ...")
+    stopApp()
+  })
+  
+  # --- Programmtests ... ----------------------------------------------------------------------------------
+  
+  # t001: Zeit und Typumwandlung
+  observeEvent(input$testBerechnung, {
+    t0 <- system.time(
+      {
+        teRe <- rep(TRUE, 4)
+        teRe <- c(teRe, FALSE)
+        teRe <- ifelse(teRe, 1, -1)        
+        teReInt <- as.integer(teRe)
+      })
+    t1 <- system.time(output$ergebnisBerechnung <- renderText({ paste(teRe)} ))
+    t2 <- system.time(output$ergebnisBerechnung2 <- renderText({ paste(teReInt)} ))
+    print(t0)
+    print(t1)
+    print(t2)
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -6,5 +6,26 @@
 library(shiny)
 
 shinyUI(fluidPage(
-
+  # --- Titel ----------------------------------------------------------------------------------------------
+  titlePanel("Schafkopf - mitgezählt ..."),
+  hr(),
+  # --- Gruppenauswahl -------------------------------------------------------------------------------------
+  wellPanel(
+    fluidRow(
+      column(2, selectInput("gruppeWaehlen", "Gruppe", c("A", "B", "C")) ),
+      column(9),
+      column(1, actionButton("neueGruppe","Neue Gruppe", width = 130),
+                actionButton("loescheGruppe", "Gruppe löschen", width = 130))
+    )
+  ),
+  
+  # --- Test-Panels ----------------------------------------------------------------------------------------
+  # Hier können verschiedene Outputs getestet werden
+  
+  # t001: Ausgabe der Umwandlungen
+  conditionalPanel("false",
+                    actionButton("testBerechnung", "Berechne"),
+                    textOutput("ergebnisBerechnung"),
+                    textOutput("ergebnisBerechnung2")
+  )
 ))
